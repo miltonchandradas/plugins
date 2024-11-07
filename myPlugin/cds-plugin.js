0fde43927af94ff31a3e342ec84c9987e2d3877f
Milton Chandradas <milton.chandradas@sap.com>
2024-11-07 01:54:30 +0000

    initial commit...

---
+++ myPlugin/cds-plugin.js
@@ -0,0 +1,31 @@
+const cds = require("@sap/cds");
+
+cds.add?.register?.(
+  "myplugin",
+  class extends cds.add.Plugin {
+    async run() {
+      console.log("MORE STUFF...");
+    }
+  }
+);
+
+console.log("My plugin is loaded...");
+cds.once("served", () => {
+  for (const srv of cds.services) {
+    for (const entity of srv.entities) {
+      for (const element in entity.elements) {
+        if (Object.hasOwnProperty.call(entity.elements, element)) {
+          const field = entity.elements[element];
+
+          if (field["@myplugin"]) {
+            console.log(field.name);
+
+            srv.after("each", entity, (item) => {
+              item[field.name] += "👍 👍 👍";
+            });
+          }
+        }
+      }
+    }
+  }
+});
